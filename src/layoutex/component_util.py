05064197a5b84d453406a9ba3f3b733888cbae0f
scstauf <scstauf@gmail.com>
2023-05-17 12:44:31 -0500

    Changed some calls: logger.info to logger.debug

--- src/layoutex/component_util.py
+++ src/layoutex/component_util.py
@@ -9,16 +9,16 @@ def estimate_component_sizing(box, target_size, margin_size):
     """
     estimate the component sizing based on the bounding box size and the target size of the document including margins
     """
-    logger.info(f"estimate_component_sizing(target_size={target_size}, margin_size={margin_size})")
+    logger.debug(f"estimate_component_sizing(target_size={target_size}, margin_size={margin_size})")
     x1, y1, x2, y2 = box
     target_size = target_size - 2 * margin_size
-    logger.info(f"computed target_size = {target_size}")
+    logger.debug(f"computed target_size = {target_size}")
     w = int(x2 - x1)
     h = int(y2 - y1)
-    logger.info(f"computed dimensions  = (w={w}, h={h})")
+    logger.debug(f"computed dimensions  = (w={w}, h={h})")
     ratio_w = w / target_size
     ratio_h = h / target_size
-    logger.info(f"computed ratios      = (ratio_w={ratio_w}, ratio_h={ratio_h})")
+    logger.debug(f"computed ratios      = (ratio_w={ratio_w}, ratio_h={ratio_h})")
 
     component_w = "FULL_WIDTH"
     component_h = "FULL_HEIGHT"
@@ -32,7 +32,7 @@ def estimate_component_sizing(box, target_size, margin_size):
     elif ratio_w > 0.01:
         component_w = "QUARTER_WIDTH"
 
-    logger.info(f"computed component_w = {component_w}")
+    logger.debug(f"computed component_w = {component_w}")
 
     if ratio_h > 0.75:
         component_h = "FULL_HEIGHT"
@@ -43,7 +43,7 @@ def estimate_component_sizing(box, target_size, margin_size):
     elif ratio_h > 0.01:
         component_h = "LINE_HEIGHT"
 
-    logger.info(f"computed component_h = {component_h}")
+    logger.debug(f"computed component_h = {component_h}")
 
     # logger.debug(f"estimated component sizing - ratio_w: {ratio_w}, ratio_h: {ratio_h}  -> {component_w}, {component_h}")
     return component_w, component_h
